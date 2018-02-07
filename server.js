87b3e42be9289eff199ebd854e7e8dd9e37c4891
Udeep <udeep@nagarro.com>
2018-02-07 14:57:16 +0530

    updated server js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+app.use((req, res, next) => {
+  if (req.protocol === 'http') {
+    next();
+  } else{
+    res.redirect('http://' + req.hostname + req.url);
+  }
+})
 app.use(express.static(__dirname + '/public'));
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
   console.log(`Server up at ${PORT}`);
 })
