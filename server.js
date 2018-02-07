3adadd30b6f7c4ea455a08f767642b7be4f0be74
Udeep <udeep@nagarro.com>
2018-02-07 14:21:51 +0530

    changed express server file

--- server.js
+++ server.js
@@ -1,15 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.use((req,res,next)=>{
-  if (req.headers['x-forwaded-proto'] === 'http' ) {
-    next();
-  } else{
-    res.redirect('http://'+ req.hostname + req.url);
-  }
-})
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.listen(PORT,()=>{
   console.log(`Server up at ${PORT}`);
 })
